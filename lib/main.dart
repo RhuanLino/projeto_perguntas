7c3f1f589eeb88095c657a5711ec5abc1c0436e0
RhuanLino <rhuanlino309@gmail.com>
2024-09-05 16:34:19 -0300

    fetch add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import ''
 
-main () {
-  runApp(new PerguntaApp());
-}
+main () => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  final perguntas = ['Qual seu animal favorito?', 'Qual sua cor favorita?']
+  
+  final perguntas = ['Qual seu animal favorito?', 'Qual sua cor favorita?'];
+
+  var perguntaSelecionada = 0;
+
+  void responder () {
+    print('Pergunta respondida!');
+  }
 
   @override
   Widget build (BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App Perguntas'),
         ),
         body: Column(
-          children: [
-            Text('Linha 1'),
-            Text('Linha 2'),
-            Text('Linha 3')
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: (responder)
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: (responder)
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: (responder)
+            )
           ],
         ),
       ),
